8a68c1e526a921ccec4b0bf016fcef6090980d97
MrRocketR <kuptsovns@gmail.com>
2023-02-05 14:26:20 +0200

    1. RESTFull. Описание архитектуры

--- src/main/java/ru/job4j/rest/config/DataConfig.java
+++ src/main/java/ru/job4j/rest/config/DataConfig.java
@@ -44,14 +44,6 @@ public class DataConfig {
         txManager.setEntityManagerFactory(entityManagerFactory);
         return txManager;
     }
-    @Bean
-    public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
-        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
-        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
-        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
-        return dataSource;
-    }
+
 
 }
